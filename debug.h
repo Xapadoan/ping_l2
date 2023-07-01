29e83609d6368a47215fe5657285ee28bfb68ad8
Xapadoan <HollyolGamu@hotmail.com>
2023-07-01 21:00:29 +0200

    Better debug

--- debug.h
+++ debug.h
@@ -7,6 +7,11 @@
 #define PL2_LOG_LVL_DEBUG 3
 #define DEBUG_LVL 3
 
+#define debug(...) (DEBUG_LVL >= PL2_LOG_LVL_DEBUG ? printf(__VA_ARGS__) : (void)0)
+#define info(...) (DEBUG_LVL >= PL2_LOG_LVL_INFO ? printf(__VA_ARGS__) : (void)0)
+#define warn(...) (DEBUG_LVL >= PL2_LOG_LVL_WARN ? printf(__VA_ARGS__) : (void)0)
+#define error(...) (DEBUG_LVL >= PL2_LOG_LVL_ERROR ? printf(__VA_ARGS__) : (void)0)
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
