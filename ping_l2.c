5814cc91a4c1a557592c00fd681b1faef8730a1c
Xapadoan <HollyolGamu@hotmail.com>
2023-07-01 21:01:18 +0200

    Get gateway hwaddr with arp

--- ping_l2.c
+++ ping_l2.c
@@ -2,9 +2,13 @@
 
 int main(int argc, char **argv)
 {
-  char  ifname[IFNAMSIZ];
+  char        ifname[IFNAMSIZ];
+  uint8_t     gw_hwaddr[ETH_ALEN];
+  const char  dst_ip[8] = "8.8.8.8";
 
   memset(ifname, 0, IFNAMSIZ);
+  memset(gw_hwaddr, 0, ETH_ALEN);
   getrecvif(ifname);
-  printf("END: %s\n", ifname);
+  sendarp(ifname, "192.168.0.254", gw_hwaddr);
+  printf("END: %02x:%02x:%02x:%02x:%02x:%02x\n", gw_hwaddr[0], gw_hwaddr[1], gw_hwaddr[2], gw_hwaddr[3], gw_hwaddr[4], gw_hwaddr[5]);
 }
