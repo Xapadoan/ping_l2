ae8877e63fa6d6ed0cea568f0a01ca8ae0f2b599
Xapadoan <HollyolGamu@hotmail.com>
2023-07-14 18:02:33 +0200

    Make getifinfo compatible with ipv6

--- getifinfo.c
+++ getifinfo.c
@@ -1,12 +1,18 @@
 #include "getifinfo.h"
 
-int getifname(char *ifname)
+int pickif(struct ifinfo *info, int family)
 {
   struct ifaddrs  *all;
   struct ifaddrs  *i;
+  int             addr_len;
 
+  addr_len = 0;
   all = NULL;
   i = NULL;
+  if (family == AF_INET)
+    addr_len = sizeof(struct sockaddr_in);
+  else if (family == AF_INET6)
+    addr_len = sizeof(struct sockaddr_in6);
   if (getifaddrs(&all) != 0)
   {
     perror("[getifname] Failed to getifaddrs");
@@ -25,14 +31,18 @@ int getifname(char *ifname)
   i = all;
   while (i != NULL)
   {
-    if ((i->ifa_flags & IFF_UP && i->ifa_flags & IFF_RUNNING) && !(i->ifa_flags & (IFF_LOOPBACK | IFF_NOARP)))
+    if ((i->ifa_flags & IFF_UP && i->ifa_flags & IFF_RUNNING) && !(i->ifa_flags & (IFF_LOOPBACK | IFF_NOARP)) && i->ifa_addr->sa_family == family)
     {
       debug("[getifname] Chose interface:\n");
       if (DEBUG_LVL >= PL2_LOG_LVL_DEBUG)
       {
         putifaddr(i);
       }
-      memcpy(ifname, i->ifa_name, IFNAMSIZ);
+      memcpy(info->name, i->ifa_name, IFNAMSIZ);
+      if (family == AF_INET)
+        info->in_addr = ((struct sockaddr_in*)i->ifa_addr)->sin_addr.s_addr;
+      else if (family == AF_INET6)
+        memcpy(&(info->in6_addr), ((struct sockaddr_in6*)i->ifa_addr)->sin6_addr.s6_addr, 16);
       freeifaddrs(all);
       return (0);
     }
@@ -43,13 +53,13 @@ int getifname(char *ifname)
   return (-1);
 }
 
-int getifinfo(struct ifinfo *info)
+int getifinfo(struct ifinfo *info, int family)
 {
   struct ifreq  ifr;
   int           fd;
 
   memset(info, 0, sizeof(struct ifinfo));
-  if (getifname(info->name) != 0)
+  if (pickif(info, family) != 0)
   {
     error("[getifinfo] getifname failed\n");
     return (-1);
@@ -75,26 +85,21 @@ int getifinfo(struct ifinfo *info)
     return (-1);
   }
   memcpy(info->hwaddr, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
-  if (ioctl(fd, SIOCGIFADDR, &ifr) == -1)
-  {
-    perror("[getifinfo] Failed to get address");
-    close(fd);
-    return (-1);
-  }
-  if (ifr.ifr_addr.sa_family != AF_INET)
-  {
-    error("[getifinfo] Interface has no AF_INET addr");
-    close(fd);
-    return (-1);
-  }
-  info->addr = ((struct sockaddr_in *)&(ifr.ifr_addr))->sin_addr.s_addr;
   
   debug(\
-    "[getifinfo] %s (%u):\n[getifinfo]\thwaddr: %02x:%02x:%02x:%02x:%02x:%02x\n[getifinfo]\taddr: %02x.%02x.%02x.%02x\n", \
+    "[getifinfo] %s (%u):\n[getifinfo]\thwaddr: %02x:%02x:%02x:%02x:%02x:%02x\n[getifinfo]\tin_addr: %u.%u.%u.%u\n", \
     info->name, info->index, \
     info->hwaddr[0], info->hwaddr[1], info->hwaddr[2], info->hwaddr[3], info->hwaddr[4], info->hwaddr[5], \
-    info->addr >> 24 & 0x000000ff, info->addr >> 16 & 0x000000ff, info->addr >> 8 & 0x000000ff, info->addr & 0x000000ff\
+    ntohl(info->in_addr) >> 24 & 0x000000ff, ntohl(info->in_addr) >> 16 & 0x000000ff, ntohl(info->in_addr) >> 8 & 0x000000ff, ntohl(info->in_addr) & 0x000000ff\
   );
+  debug("[getifinfo]\tin6_addr: ");
+  int i = 0;
+  while (i < 16)
+  {
+    printf("%02x", info->in6_addr[i]);
+    i++;
+  }
+  debug("\n");
   close(fd);
   return (0);
 }
--- getifinfo.h
+++ getifinfo.h
@@ -17,9 +17,10 @@ struct ifinfo {
   int       index;
   char      name[IFNAMSIZ];
   uint8_t   hwaddr[ETH_ALEN];
-  uint32_t  addr;
+  uint32_t  in_addr;
+  uint8_t   in6_addr[16];
 };
 
-int getifinfo(struct ifinfo *info);
+int getifinfo(struct ifinfo *info, int family);
 
 #endif
