9510ab062077184338f204bfa5adbb0d40f11e6d
Xapadoan <HollyolGamu@hotmail.com>
2023-07-23 10:03:14 +0200

    Use args

--- ping_l2.c
+++ ping_l2.c
@@ -5,7 +5,6 @@ int main(int argc, char **argv)
   uint8_t     gw_hwaddr[ETH_ALEN];
   uint32_t    gw_ip;
   const char  dst_ip[8] = "8.8.8.8";
-  int         family = AF_INET;
   int         sock;
   uint8_t     packet[1500];
   struct ifinfo local;
@@ -13,12 +12,11 @@ int main(int argc, char **argv)
   struct sockaddr_in6 dst;
   struct args args;
 
-  if (parseargs(argc, argv, &parsed) != 0)
+  if (parseargs(argc, argv, &args) != 0)
   {
     error("[ping_l2] parseargs failed\n");
     return (-1);
   }
-  /*
   gw_ip = 0;
   memset(gw_hwaddr, 0, ETH_ALEN);
   memset(&local, 0, sizeof(local));
@@ -33,10 +31,10 @@ int main(int argc, char **argv)
     error("[ping_l2] getifinfo failed\n");
     return (-1);
   }
-  if (family == AF_INET6)
+  if (args.family == AF_INET6)
   {
     dst.sin6_family = AF_INET6;
-    if (inet_pton(family, "2001:4860:4860::8888", &dst.sin6_addr) != 1)
+    if (inet_pton(args.family, args.target, &dst.sin6_addr) != 1)
     {
       error("[ping_l2] inet_pton failed\n");
       return (-1);
@@ -51,7 +49,7 @@ int main(int argc, char **argv)
       error("[ping_l2] gatewayinfo failed\n");
       return (-1);
     }
-  } else if (family == AF_INET) {
+  } else if (args.family == AF_INET) {
     if (bindsock(&sock, local.index, ETH_P_IP) != 0)
     {
       error("[ping_l2] bindsock failed\n");
@@ -65,20 +63,19 @@ int main(int argc, char **argv)
     }
   }
   debug("[ping_l2] GW_HWADDR: %02x:%02x:%02x:%02x:%02x:%02x\n", gw_hwaddr[0], gw_hwaddr[1], gw_hwaddr[2], gw_hwaddr[3], gw_hwaddr[4], gw_hwaddr[5]);
-  if (family == AF_INET6)
+  if (args.family == AF_INET6)
   {
     buildether(packet, local.hwaddr, gw_hwaddr, ETH_P_IPV6);
-    buildipv6(packet + ETH_HLEN, local.in6_addr, dst.sin6_addr.s6_addr, 20);
-    buildicmpv6(packet + ETH_HLEN, 128, 1, 20);
-    sendether(sock, local.index, packet, 74, PACKET_OUTGOING);
+    buildipv6(packet + ETH_HLEN, local.in6_addr, dst.sin6_addr.s6_addr, &args.ipopts.v6);
+    buildicmpv6(packet + ETH_HLEN, 128, 1, 40 + args.ipopts.v6.plen);
+    sendether(sock, local.index, packet, ETH_HLEN + 40 + args.ipopts.v6.plen, PACKET_OUTGOING);
   }
-  if (family == AF_INET)
+  if (args.family == AF_INET)
   {
     buildether(packet, local.hwaddr, gw_hwaddr, ETH_P_IP);
-    buildip(packet + ETH_HLEN, local.in_addr, inet_addr(dst_ip), 20);
-    buildicmp(packet + ETH_HLEN + 20, 8, 1, 20);
-    sendether(sock, local.index, packet, 54, PACKET_OUTGOING);
+    buildip(packet + ETH_HLEN, local.in_addr, inet_addr(args.target), &args.ipopts.v4);
+    buildicmp(packet + ETH_HLEN + args.ipopts.v4.len - sizeof(struct icmphdr) - args.size, 8, 1, args.size);
+    sendether(sock, local.index, packet, ETH_HLEN + args.ipopts.v4.len, PACKET_OUTGOING);
   }
-  */
   return (0);
 }
